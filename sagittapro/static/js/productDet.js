5433f283a65c5b39bb1da783fe78774b7047af45
salemsifeddine <saiftony5@gmail.com>
2021-12-22 13:08:17 +0100

    ftoor

--- sagittapro/static/js/productDet.js
+++ sagittapro/static/js/productDet.js
@@ -4,9 +4,13 @@ var divmoreDet = document.querySelector(".moredetDiv")
 moredetbtn.addEventListener('click',function(){
     this.classList.toggle("showmore");
     if(this.classList.contains("showmore")){
-        divmoreDet.style.display="block"
+        // divmoreDet.style.display="block"
+        $(".moredetDiv").fadeIn(400)
     }else{
-        divmoreDet.style.display="none"
+        // divmoreDet.style.display="none"
+        // $(".moredetDiv").fadeOut(200)
+        // $(".moredetDiv").animate({"height":0+'%'},500)
+        $(".moredetDiv").fadeOut(300)
     }
     
 })
