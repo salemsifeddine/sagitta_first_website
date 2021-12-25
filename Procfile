bf7140238121bceeb28d6bfabc5a157bb2b75e23
salemsifeddine <saiftony5@gmail.com>
2021-12-25 15:46:01 +0100

    'settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sagitta.wsgi --log-file -
--- sagitta/settings.py
+++ sagitta/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sagitta.urls'
@@ -129,3 +130,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
