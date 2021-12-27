78384f8ec8577836a7deeed2ebef2a15aed04d8e
salemsifeddine <saiftony5@gmail.com>
2021-12-27 15:34:08 +0100

    today

--- sagittapro/urls.py
+++ sagittapro/urls.py
@@ -5,7 +5,5 @@ from django.contrib.auth import views as view
 urlpatterns = [
     path('', views.main, name="main"),
     path('produits/', views.products, name="product"),
-    path('produitDet/<int:pk>',  views.ProductDetailView.as_view(), name="productDet"),
     path('faq/', views.faq, name="faq"),
-    path('product/<int:pk>', views.ProductDetailView.as_view(), name="panier"),
 ]
