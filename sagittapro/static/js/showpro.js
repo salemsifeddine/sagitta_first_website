9b47a9b3c8c19089a2013e4538c71128778e3517
salemsifeddine <saiftony5@gmail.com>
2022-01-22 00:52:59 +0100

    pro

--- sagittapro/static/js/showpro.js
+++ sagittapro/static/js/showpro.js
@@ -9,8 +9,29 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       type: "fraction" } });
   
-  
+  var datastl={
+    "0":"#b6d6c8",
+    "1":"#e9bf8b",
+    "2":"#b6d6c8",
+    "3":"#e86357",
+    
+  }
   
   swiper.on('slideChange', function (sld) {
     document.body.setAttribute('data-sld', sld.realIndex);
+  
+
+    document.head.children[document.head.childElementCount - 1].remove()
+    var stl=  document.createElement("style")
+    var textstl=document.createTextNode(`[data-sld="${sld.realIndex}"] .container, body {
+      background-color: ${datastl[sld.realIndex]} ;
+    }`)
+    stl.append(textstl)
+    document.head.appendChild(stl)
+    
   });
+
+
+ 
+  
+  
