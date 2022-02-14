04d14d57e4a1b1c79fee7b4815049a9553c526f6
salemsifeddine <saiftony5@gmail.com>
2022-02-14 11:21:13 +0100

    img size

--- sagittapro/views.py
+++ sagittapro/views.py
@@ -41,50 +41,11 @@ def contact(request):
     context={"title":"contact","form":form}
     return render(request,"pages/contact.html",context)
 
-
-# from django.views import generic
-# class ProductDetailView(generic.DetailView):
-    
-    
-#     model=Product
-#     template_name="pages/test.html"
-    # def get_context_data(self,**kwargs):
-
-      
-    #     total = super().get_context_data(**kwargs)
-    #     products = super().get_context_data(**kwargs)
-        
-    #     total["relatedProducts"]= Product.objects.all().filter(catigory=self.object.catigory.pk)[:6]
-    #     total["products"]=Product.objects.all()
-    #     total["colors"]=Color.objects.all()
-    #     total["sizes"]=Size.objects.all()
-    #     total["totalitem"]=callcartnumber(self.request)['total']
-    #     try:
-    #         total["wishlist"]=callwishnumber(self.request)
-    #     except:
-    #         total["wishlist"]=[]
-        
-    #     if self.request.user.is_authenticated:
-    #         total["wishlists"]=Wishlist.objects.all().filter(customer=self.request.user)
-    #         total['wishedproduct']=Wishlist.objects.all().filter(customer=self.request.user, product=self.object.pk)
-
-    #         print(total['wishedproduct'])
-            
-    #     total['related']=Product.objects.all().filter(catigory=self.object.pk)
-
-    #     try:
-    #         total['wishedproduct']=total['wishedproduct'].first().product
-    #     except:
-    #         total['wishedproduct']=''
-
-       
-    #     return total
-
+ 
 
 
 def faq(request):
     context={
-        # "faqs":FaQ.objects.all(),
         "title":"F&Q"
     }
     return render(request,"pages/faq.html",context)
